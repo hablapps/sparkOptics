fecdd8e34b62429d7b03f06cd9c8c3c146d99bef
Alfonso Roa <roaredalfonso@gmail.com>
2019-04-09 00:52:08 +0200

    All input dataframes to dataset[_]

--- src/main/scala/org/hablapps/sparkOptics/GlassesFrame.scala
+++ src/main/scala/org/hablapps/sparkOptics/GlassesFrame.scala
@@ -1,7 +1,7 @@
 package org.hablapps.sparkOptics
 
 import org.apache.spark.sql.types.StructField
-import org.apache.spark.sql.{Column, DataFrame}
+import org.apache.spark.sql.{Column, DataFrame, Dataset}
 import org.hablapps.sparkOptics.ProtoLens.ProtoLens
 
 object GlassesFrame {
@@ -12,19 +12,19 @@ object GlassesFrame {
 
     implicit class GlassesProtoLensSyntax[O](optic: O)(
         implicit glasses: GlassesFrame[O]) {
-      def setDF(newValue: Column): DataFrame => DataFrame =
+      def setDF(newValue: Column): Dataset[_] => DataFrame =
         glasses.set(optic)(newValue)
 
-      def setDFCheckingSchema(newValue: Column): DataFrame => DataFrame =
+      def setDFCheckingSchema(newValue: Column): Dataset[_] => DataFrame =
         glasses.setAndCheckSchema(optic)(newValue)
 
-      def getDF: DataFrame => Column =
+      def getDF: Dataset[_] => Column =
         glasses.get(optic)
 
-      def modifyDF(f: Column => Column): DataFrame => DataFrame =
+      def modifyDF(f: Column => Column): Dataset[_] => DataFrame =
         glasses.modify(optic)(f)
 
-      def modifyDFCheckingSchema(f: Column => Column): DataFrame => DataFrame =
+      def modifyDFCheckingSchema(f: Column => Column): Dataset[_] => DataFrame =
         glasses.modifyAndCheckSchema(optic)(f)
     }
 
@@ -34,28 +34,28 @@ object GlassesFrame {
     implicit val lensGlasses: GlassesFrame[Lens] =
       new GlassesFrame[Lens] {
         override def set(optic: Lens)(
-            newValue: Column): DataFrame => DataFrame = {
+            newValue: Column): Dataset[_] => DataFrame = {
           _.select(optic.set(newValue): _*)
         }
 
-        override def get(optic: Lens): DataFrame => Column = _ => optic.get
+        override def get(optic: Lens): Dataset[_] => Column = _ => optic.get
 
         override def modify(optic: Lens)(
-            f: Column => Column): DataFrame => DataFrame =
+            f: Column => Column): Dataset[_] => DataFrame =
           _.select(optic.modify(f): _*)
       }
 
     implicit val protoLensGlasses: GlassesFrame[ProtoLens] =
       new GlassesFrame[ProtoLens] {
         override def set(optic: ProtoLens)(
-            newValue: Column): DataFrame => DataFrame =
+            newValue: Column): Dataset[_] => DataFrame =
           df => lensGlasses.set(optic(df.schema))(newValue)(df)
 
-        override def get(optic: ProtoLens): DataFrame => Column =
+        override def get(optic: ProtoLens): Dataset[_] => Column =
           df => lensGlasses.get(optic(df.schema))(df)
 
         override def modify(optic: ProtoLens)(
-            f: Column => Column): DataFrame => DataFrame =
+            f: Column => Column): Dataset[_] => DataFrame =
           df => lensGlasses.modify(optic(df.schema))(f)(df)
 
       }
@@ -65,8 +65,8 @@ object GlassesFrame {
 
 trait GlassesFrame[A] {
 
-  private def modifiedFieldIsEqualType(df1: DataFrame,
-                                       df2: DataFrame,
+  private def modifiedFieldIsEqualType(df1: Dataset[_],
+                                       df2: Dataset[_],
                                        optic: A): Either[String, Unit] = {
     compareFields(df1.select(get(optic)(df1)).schema.fields.head,
                   df2.select(get(optic)(df2)).schema.fields.head)
@@ -97,13 +97,13 @@ trait GlassesFrame[A] {
         .right
     } yield ()
 
-  def set(optic: A)(newValue: Column): DataFrame => DataFrame
+  def set(optic: A)(newValue: Column): Dataset[_] => DataFrame
 
-  def get(optic: A): DataFrame => Column
+  def get(optic: A): Dataset[_] => Column
 
-  def modify(optic: A)(f: Column => Column): DataFrame => DataFrame
+  def modify(optic: A)(f: Column => Column): Dataset[_] => DataFrame
 
-  def setAndCheckSchema(optic: A)(newValue: Column): DataFrame => DataFrame =
+  def setAndCheckSchema(optic: A)(newValue: Column): Dataset[_] => DataFrame =
     df => {
       val newDf = set(optic)(newValue)(df)
       val validation = modifiedFieldIsEqualType(df, newDf, optic)
@@ -115,7 +115,7 @@ trait GlassesFrame[A] {
     }
 
   def modifyAndCheckSchema(optic: A)(
-      f: Column => Column): DataFrame => DataFrame =
+      f: Column => Column): Dataset[_] => DataFrame =
     df => {
       val newDf = modify(optic)(f)(df)
       val validation = modifiedFieldIsEqualType(df, newDf, optic)
